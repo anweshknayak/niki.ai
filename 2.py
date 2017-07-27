f7d9efc058d8f2275f44311670333608626d9ff0
anweshknayak <anweshknayak@gmail.com>
2017-07-27 05:47:09 +0530

    small bug

--- 2.py
+++ 2.py
@@ -8,11 +8,17 @@ def cipher(m,n):
     for i in range(m,n+1):
         if(i>26):
             x = i%26
-            print arr[0] + arr[x-1]
+            y = i/26
+
+            if(x==0):
+                print arr[y-2] + arr[x-1]
+           
+            else:
+                print arr[y-1] + arr[x-1]
 
         else:
             print arr[i-1]
 
 
-#if __name__ == '__main__':
-#    cipher(25,29)
+if __name__ == '__main__':
+    cipher(25,100)
